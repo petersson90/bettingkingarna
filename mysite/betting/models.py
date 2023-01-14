48645baceff7cdbdf7f891554546ac54393baa1f
Jonas Petersson <jonas.eric.peterson@gmail.com>
2023-01-15 00:28:58 +0100

    Added Game to the model

--- mysite/betting/models.py
+++ mysite/betting/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
