a6fd07227c36c97715b548b9e44fa4baa4ea030e
Jonas Petersson <jonas.eric.peterson@gmail.com>
2023-01-15 00:52:49 +0100

    Added __str__ for Game

--- mysite/betting/models.py
+++ mysite/betting/models.py
@@ -5,3 +5,6 @@ class Game(models.Model):
     home_team = models.CharField(max_length=100)
     away_team = models.CharField(max_length=100)
     start_time = models.DateTimeField()
+    
+    def __str__(self):
+        return f'{self.home_team} - {self.away_team}'
