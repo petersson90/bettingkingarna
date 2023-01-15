6e1cc08373e7f76f069d5a7228fdb6380c43ea54
Jonas Petersson <jonas.eric.peterson@gmail.com>
2023-01-15 23:28:56 +0100

    Added model for Team

--- mysite/betting/models.py
+++ mysite/betting/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    short = models.CharField(max_length=4)
+    
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Game(models.Model):
     home_team = models.CharField(max_length=100)
     away_team = models.CharField(max_length=100)
