1611419a144ad2ff0f72fcdb2f9829b8d01b2082
Jonas Petersson <jonas.eric.peterson@gmail.com>
2023-02-13 21:31:14 +0100

    Static file configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 from django.contrib.messages import constants as messages
@@ -123,6 +124,8 @@ FORMAT_MODULE_PATH = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
