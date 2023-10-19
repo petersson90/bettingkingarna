d6f0e9e383b9c1e945a4c7ff93e63619330f741b
Jonas Petersson <jonas.eric.peterson@gmail.com>
2023-10-19 12:08:32 +0200

    Fields included in admin interface

--- betting/admin.py
+++ betting/admin.py
@@ -3,7 +3,7 @@ from .models import Competition, Team, Game, Bet, StandingPrediction
 
 # Register your models here.
 class CompetitionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'season')
+    list_display = ['name', 'season']
 
 admin.site.register(Competition, CompetitionAdmin)
 
@@ -16,12 +16,12 @@ admin.site.register(Team, TeamAdmin)
 
 class BetInline(admin.TabularInline):
     model = Bet
-    fields = ('game', 'user', 'result')
-    readonly_fields = ('game', 'user', 'result')
+    fields = ['game', 'user', 'result', 'is_updated', 'points']
+    readonly_fields = fields
     extra = 0
 
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('start_time', 'home_team', 'away_team', 'result')
+    list_display = ['start_time', 'home_team', 'away_team', 'result']
     list_filter = ['start_time', 'competition', 'home_team', 'away_team']
     inlines = [BetInline]
 
@@ -29,13 +29,13 @@ admin.site.register(Game, GameAdmin)
 
 
 # class BetAdmin(admin.ModelAdmin):
-#     list_display = ('game', 'user', 'result', 'points')
+#     list_display = ['game', 'user', 'result', 'points']
 #     list_filter = ['user']
 
 # admin.site.register(Bet, BetAdmin)
 
 class StandingPredictionAdmin(admin.ModelAdmin):
-    list_display = ('competition', 'user')
+    list_display = ['competition', 'user']
     list_filter = ['user']
 
 admin.site.register(StandingPrediction, StandingPredictionAdmin)
