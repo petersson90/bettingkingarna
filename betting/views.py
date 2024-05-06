859791715dc6947fdf2671a7473b60b9cc42e2f6
Jonas Petersson <jonas.eric.peterson@gmail.com>
2024-05-06 22:34:52 +0200

    Updated standings after round 7

--- betting/views.py
+++ betting/views.py
@@ -12,9 +12,9 @@ from accounts.models import CustomUser
 from .forms import TeamForm, GameForm, BetForm, StandingPredictionForm, TableBetForm
 from .models import Team, Competition, Game, Bet, StandingPrediction, StandingPredictionTeam
 
-ALLSVENSKAN_2024 = '1,29,7,3,15,23,11,6,26,30,22,13,18,5,33,4'
+ALLSVENSKAN_2024 = '1,7,3,23,26,29,15,18,11,30,6,22,13,5,4,33'
 TOP_SCORER_2024 = 'Isaac Kiese Thelin'
-MOST_ASSISTS_2024 = 'Sebastian Nanasi, Nikola Vasic, Vinicius Nogueira, Thomas Santos, Amor Layouni & Erik Botheim'
+MOST_ASSISTS_2024 = 'Vinícius Nogueira & Jacob Trenskow'
 DEADLINE_2024 = timezone.make_aware(timezone.datetime(2024, 4, 7, 11))
 ALLSVENSKAN_2023 = '1,18,23,3,5,4,6,13,11,15,7,29,22,30,8,24'
 TOP_SCORER_2023 = 'Isaac Kiese Thelin'
